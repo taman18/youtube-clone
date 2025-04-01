863d3fee2f288aaf91a8c759a6648ea98de96246
Tamanjeet <tamanjeet1@appinventiv.com>
2025-04-01 16:23:45 +0530

    added routes, middlewares

--- src/app.js
+++ src/app.js
@@ -23,4 +23,10 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+//routes
+
+import userRouter from '../src/routes/user.routes.js';
+
+app.use("/api/v1/users", userRouter);
+
 export { app };
---
+++ utils/cloudinary.js
@@ -0,0 +1,25 @@
+import {v2 as cloudinary} from cloudinary;
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+  // Upload an image
+const uploadCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        // Upload the image to Cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+        });
+        console.log(response.url);
+        return response;
+    }
+    catch (err) {
+        fs.unlinkSync(localFilePath);
+        throw err;
+    }
+}
